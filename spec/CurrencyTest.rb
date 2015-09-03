f4771cddbc6ffc6fac1c3c9f718911b65dd55805
Ryan Yep <ryan.yep@gmail.com>
2015-09-03 16:15:34 -0500

    first test for Currency Object passed

---
+++ spec/CurrencyTest.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/CurrencyClass.rb'
+
+class CurrencyClassTest < Minitest::Test
+
+  def test_currencyclass_exists
+    assert(Currency) #checks to see if class exists in file
+  end
+
+end
