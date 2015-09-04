c97adcb90fb8f730bca00d4829ddda6156b9faf5
Ryan Yep <ryan.yep@gmail.com>
2015-09-04 16:03:27 -0500

    working multiply method w/ test

--- lib/CurrencyClass.rb
+++ lib/CurrencyClass.rb
@@ -18,26 +18,33 @@ class Currency
     @ccode = ccode
   end
 
-  def ==(other)
-    @amt_to_convert == other.amt_to_convert && @ccode == other.ccode
+  def ==(equalto)
+    @amt_to_convert == equalto.amt_to_convert && @ccode == equalto.ccode
   end
 
-  def !=(other)
-    !(@amt_to_convert == other.amt_to_convert && @ccode == other.ccode)
+  def !=(notequal)
+    !(@amt_to_convert == notequal.amt_to_convert && @ccode == notequal.ccode)
   end
 
-  def +(other)
-    if other.is_a?(Currency)
-      raise DifferentCurrencyCodeError if @ccode != other.ccode
-      Currency.new(@amt_to_convert + other.amt_to_convert, @ccode)
+  def +(add)
+    if add.is_a?(Currency)
+      raise DifferentCurrencyCodeError if @ccode != add.ccode
+      Currency.new(@amt_to_convert + add.amt_to_convert, @ccode)
     end
   end
 
-  def -(other)
-    if other.is_a?(Currency)
-      raise DifferentCurrencyCodeError if @ccode != other.ccode
-      Currency.new(@amt_to_convert - other.amt_to_convert, @ccode)
+  def -(minus)
+    if minus.is_a?(Currency)
+      raise DifferentCurrencyCodeError if @ccode != minus.ccode
+      Currency.new(@amt_to_convert - minus.amt_to_convert, @ccode)
     end
   end
 
+  def *(multiply)
+    if multiply.is_a?(Currency)
+      raise DifferentCurrencyCodeError if multiply.is_a?(Float) == false &&
+      multiply._is_a?(Fixnum) == false
+      Currency.new(@amt_to_convert * multiply, @ccode)
+    end
+  end
 end
--- spec/CurrencyTest.rb
+++ spec/CurrencyTest.rb
@@ -6,7 +6,7 @@ require_relative '../lib/CurrencyClass.rb'
 class CurrencyClassTest < Minitest::Test
 
   def test_currency_exists
-    assert(Currency) #checks to see if class exists in file
+    assert(Currency)
   end
 
   def test_plus_method
@@ -26,7 +26,7 @@ class CurrencyClassTest < Minitest::Test
   end
 
   def test_custom_error
-    assert(DifferentCurrencyCodeError)
+    assert(DifferentCurrencyCodeError)  # => true
   end
 
   def test_not_equal_method
@@ -40,30 +40,38 @@ class CurrencyClassTest < Minitest::Test
   def test_minus_method
     currency       = Currency.new(75, "USD")
     currency2      = Currency.new(50, "USD")
-    expected       = Currency.new(25, "USD")
-    actual         = currency - currency2
-    assert_equal(expected, actual)
+    expected       = Currency.new(25, "USD")  
+    actual         = currency - currency2     # => #<Currency:0x007feded0b1f20 @amt_to_convert=25, @ccode="USD">
+    assert_equal(expected, actual)            # => true
 
-    currency       = Currency.new(25, "USD")
-    currency2      = Currency.new(50, "USD")
-    expected       = Currency.new(-25, "USD")
-    actual         = currency - currency2
-    assert_equal(expected, actual)
+    currency       = Currency.new(25, "USD")   # => #<Currency:0x007feded0b18e0 @amt_to_convert=25, @ccode="USD">
+    currency2      = Currency.new(50, "USD")   # => #<Currency:0x007feded0b1660 @amt_to_convert=50, @ccode="USD">
+    expected       = Currency.new(-25, "USD")  # => #<Currency:0x007feded0b1368 @amt_to_convert=-25, @ccode="USD">
+    actual         = currency - currency2      # => #<Currency:0x007feded0b1098 @amt_to_convert=-25, @ccode="USD">
+    assert_equal(expected, actual)             # => true
   end
 
   def test_plus_with_error
-    currency       = Currency.new(75, "USD")
-    currency2      = Currency.new(25, "GPD")
+    currency       = Currency.new(75, "USD")      # => #<Currency:0x007feded0bbca0 @amt_to_convert=75, @ccode="USD">
+    currency2      = Currency.new(25, "GPD")      # => #<Currency:0x007feded0bb908 @amt_to_convert=25, @ccode="GPD">
     assert_raises(DifferentCurrencyCodeError) do
       total_amt = currency + currency2
-    end
+    end                                           # => #<DifferentCurrencyCodeError: DifferentCurrencyCodeError>
   end
 
   def test_minus_with_error
-    currency       = Currency.new(100, "USD")
-    currency2      = Currency.new(25, "GPD")
+    currency       = Currency.new(100, "USD")     # => #<Currency:0x007feded0b00f8 @amt_to_convert=100, @ccode="USD">
+    currency2      = Currency.new(25, "GPD")      # => #<Currency:0x007feded0b0f80 @amt_to_convert=25, @ccode="GPD">
     assert_raises(DifferentCurrencyCodeError) do
       total_amt = currency - currency2
-    end
+    end                                           # => #<DifferentCurrencyCodeError: DifferentCurrencyCodeError>
+  end
+
+  def test_multiply_method
+    currency       = Currency.new(10, "USD")             # => #<Currency:0x007feded0b99a0 @amt_to_convert=10, @ccode="USD">
+    multiple       = 3                                   # => 3
+    expected       = 30                                  # => 30
+    actual         = currency.amt_to_convert * multiple  # => 30
+    assert_equal(30, actual)                             # => true
   end
 end
