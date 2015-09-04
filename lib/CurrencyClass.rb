38223faf359821fcc75068341c74120bc1842f34
Ryan Yep <ryan.yep@gmail.com>
2015-09-04 11:55:23 -0500

    working plus, equal and custom error tests

--- lib/CurrencyClass.rb
+++ lib/CurrencyClass.rb
@@ -1,10 +1,29 @@
-class Currency
-  attr_reader :amt, :ccode
+class DifferentCurrencyCodeError < StandardError
+  # def message
+  #   puts "You're trying to compare two different currencies. Please try again."
+  # end
+end
 
-  def initialize(amt, ccode)
+class Currency
+  attr_reader :amt_to_convert, :ccode
+
+  def initialize(amt_to_convert, ccode)
     @amt_to_convert = amt_to_convert
     @ccode = ccode
   end
 
+  def ==(other)  #this is required to make the "No visible difference" error go away
+    @amt_to_convert == other.amt_to_convert && @ccode == other.ccode
+  end
+
+  def +(other)
+    if other.is_a?(Currency)
+      total_amt = Currency.new(@amt_to_convert + other.amt_to_convert, @ccode)
+      total_amt
+    else
+      puts "Error!"
+    end
+    # Currency.new(@amt_to_convert + other.amt_to_convert, @ccode)
+  end
 
 end
--- spec/CurrencyTest.rb
+++ spec/CurrencyTest.rb
@@ -9,5 +9,23 @@ class CurrencyClassTest < Minitest::Test
     assert(Currency) #checks to see if class exists in file
   end
 
+  def test_plus_method
+    currency       = Currency.new(1, "USD") #given i have a currency
+    other_currency = Currency.new(5, "USD") #when i add another currency
+    expected       = Currency.new(6, "USD")
+    actual         = (currency + other_currency)
+    assert_equal(expected, actual)
+  end
 
+  def test_equal_method
+    currency       = Currency.new(100, "USD")
+    other_currency = Currency.new(100, "USD")
+    expected       = Currency.new(100, "USD") == Currency.new(100, "USD")
+    actual         = currency == other_currency
+    assert_equal(expected, actual)
+  end
+
+  def test_custom_error
+    assert(DifferentCurrencyCodeError)
+  end
 end
